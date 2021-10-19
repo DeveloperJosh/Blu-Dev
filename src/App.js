c1735fe0edc464eda78bf8b82cc4f18ef7cf028c
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-10-18 22:32:39 -0400

    help

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello I'm Blue.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/DeveloperJosh"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github
+        </a>
+        <a
+          className="App-link"
+          href="https://mail-hook.site/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Bot
         </a>
       </header>
     </div>
