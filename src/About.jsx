ff7476e2eb58026d7d904949dd36cf75bcaef0ab
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-10-19 15:42:06 -0400

    yes

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello I'm Blue.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default About;
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from './App';
+import About from './About';
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Router exact path="/" component={App} />
+                <Router exact path="/about" component={About} />
             </Switch>
         </BrowserRouter>
     )
