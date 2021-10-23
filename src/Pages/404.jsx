70b5f827cb922b29879dfd681449463d9f60d06c
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-10-23 02:24:35 -0400

    aaaaaaaaaaaaaaaaaaaaaaaa

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -2,20 +2,22 @@ import '../App.css';
 
 export const Oh404 = () => {
     return (
+
         <div className="App">
             <header className="App-header">
-            <p>404</p>
-            <p>
-               Hey you should go home
-            </p>
+            <img id="image" src="https://i.pinimg.com/originals/ff/28/2f/ff282fc9be15259038a84650e7a83487.gif" alt="cry"/>
+            <h1 className="Home">404</h1>
+            <h1 className="Home">Hey you should go home</h1>
             <a
           className="App-link"
           href="/"
           rel="noopener noreferrer"
            >
-            <button className='btn'>Home</button>
+            <button className='btn'>Don't Cry..</button>
             </a>
+            <button className='btn' onClick="https://c.tenor.com/bBRCCeAYPU8AAAAC/cookie-mashiro.gif">Give Cookie</button><br></br>
             </header>
+
         </div>
     );
 }
