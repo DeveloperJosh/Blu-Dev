bf47a10653b2e2198489158ee585cff36b8a2224
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-10-22 22:20:30 -0400

    trying sometime

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import App from './App';
 import Oh404 from './Pages/404';
 import About from './Pages/About';
+import Order from './Pages/Store';
 
 export const Router = () => {
     return (
@@ -9,6 +10,7 @@ export const Router = () => {
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/about" component={About} />
+                <Router path="/store" component={Order} />
                 <Route component={Oh404} />
             </Switch>
         </BrowserRouter>
