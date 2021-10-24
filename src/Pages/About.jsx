5536c9e34878a25a1e790b01a0e733d7b76478dc
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2021-10-24 13:34:04 -0400

    AAAAA

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,8 +6,19 @@ function About() {
             <header className="App-header">
             <h1>About</h1>
             <p>
-               So you know my name is <b>Blue</b> and I am a <b>React/Bot Developer</b> from <b>Canada</b>.
+               So you know my name is <b>Blue</b> and I'm a <b>React/Bot Developer</b> from <b>Canada</b>.
             </p>
+            <h2 className="App-subheader">What i have made.</h2>
+            <a className="App-link"
+          href="Https://mail-hook.xyz"
+          rel="noopener noreferrer">
+          <button class className="btn blue">Mail-Hook</button>
+           </a>
+           <a className="App-link"
+          href="https://github.com/DeveloperJosh/moderextor"
+          rel="noopener noreferrer">
+          <button class className="btn red1">Moderextor</button>
+           </a>
             <a
           className="App-link"
           href="/"
